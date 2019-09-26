4b51961702e2d8e566b031bf99ce81c534acc3b7
Cody Lee <codylee02@gmail.com>
2019-09-26 14:45:08 -0600

    prepared for deployment

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const cors = require("cors");
 const MOVIES = require("./movies-data-small.json");
 
 const app = express();
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
 
-app.use(morgan("dev"));
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
@@ -20,6 +21,16 @@ app.use(function validateBearerToken(req, res, next) {
   }
   next();
 });
+
+app.use((error, req, res, next) => {
+  let response
+  if(process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
 
 app.get("/movie", function handleGetMovies(req, res) {
   let response = MOVIES;
@@ -45,8 +56,8 @@ app.get("/movie", function handleGetMovies(req, res) {
   res.json(response);
 });
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+  
 });
